4472e561a1deb5871e5bee4df9ea295c3daadb82
madhu799 <73730380+madhu799@users.noreply.github.com>
2021-06-29 17:39:05 +1000

    Add files via upload

---
+++ ReadStoryScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+
+export default class ReadStoryScreen extends React.Component {
+    render() {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>🆁🅴🅰🅳 🅰 🅱🅾🅾🅺</Text>
+        </View>
+      );
+    }
+  }
